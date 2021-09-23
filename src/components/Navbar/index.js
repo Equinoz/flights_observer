ae58292a044f13eebf644bd9bbfe55ec9ff87378
Equinoz <julien.lecoz33@gmail.com>
2021-09-23 10:28:53 +0200

    Homepage: adding navbar buttons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,31 @@
+import ButtonNavbar from "../ButtonNavbar";
+
 import "./navbar.scss";
 
-const Navbar = () => <div className="navbar">
-	<div className="navbar--desktop">
-		<p>Search flights</p>
-		<p>by...</p>
-		<ul>
-			<li>Button 1</li>
-			<li>Button 2</li>
-			<li>Button 3</li>
-			<li>Button 4</li>
-			<li>Button 5</li>
-		</ul>
-	</div>
-</div>;
+const Navbar = () => {
+	const buttons = [
+		{ icon: "globe", text: "Area" },
+		{ icon: "clock", text: "Time interval" },
+		{ icon: "plane-departure", text: "Departures by airport" },
+		{ icon: "plane-arrival", text: "Arrivals by airport" },
+		{ icon: "plane", text: "Aircraft" }
+	];
+
+	return (
+		<div className="navbar">
+			<div className="navbar--desktop">
+				<p>Search flights</p>
+				<p>by...</p>
+				<ul>
+					{ buttons.map((button, index) => <ButtonNavbar
+						key={ index }
+						icon={ button.icon }
+						text={ button.text }
+					/>) }
+				</ul>
+			</div>
+		</div>
+	);
+}
 
 export default Navbar;
