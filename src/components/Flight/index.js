714f15ee8688d69fa0b05e2ff06b2735a7673205
Equinoz <julien.lecoz33@gmail.com>
2021-09-23 12:02:50 +0200

    Homepage: flight's cards

---
+++ src/components/Flight/index.js
@@ -0,0 +1,25 @@
+import "./flight.scss";
+
+const Flight = ({ state }) => {
+	const lastContact = () => `${new Date(state[4] * 1000).toISOString().slice(0, 16).replace("T", " ")} GMT`
+
+	return (
+		<div className="flight">
+			<h3>Flight n°<span>{ state[0] }</span></h3>
+			<p><i className="fas fa-plane"></i>Aircraft identifiant: { state[1] }</p>
+			<p><i className="fas fa-flag"></i>Origin country: { state[2] }</p>
+			<div className="position">
+				<i className="fas fa-globe"></i>
+				<div>
+					<p>Longitude: { state[5].toFixed(2) }°</p>
+					<p>Latitude: { state[6].toFixed(2) }°</p>
+					<p>Altitude: { parseInt(state[13], 10) } m</p>
+				</div>
+			</div>
+			<p><i className="fas fa-tachometer-alt"></i>Speed: { parseInt(state[9] * 3.6, 10) } km/H</p>
+			<p><i className="fas fa-headset"></i>Last contact at: { lastContact() }</p>
+		</div>
+	);
+}
+
+export default Flight;
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,9 +1,19 @@
+import Flight from "../Flight";
+
 import "./main.scss";
 
-const Main = () => <div className="main">
-	<div className="container">
-		Flights
-	</div>
-</div>;
+import data from "../data.js";
+
+const Main = () => {
+	const dummiesData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22];
+
+	return (
+		<div className="main">
+			<div className="container">
+				{ dummiesData.map(i => <Flight state={ data.state } />) }
+			</div>
+		</div>
+	);
+};
 
 export default Main;
