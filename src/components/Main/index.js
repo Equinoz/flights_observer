61fdb5d2b5653ce446a95f73e9fa393783ea5a9a
Equinoz <julien.lecoz33@gmail.com>
2021-09-24 09:06:40 +0200

    Store: 	Updating README.md 	Adding comments for main 	Adding environment variables

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -5,14 +5,17 @@ import Flight from "../Flight";
 import "./main.scss";
 
 const Main = ({ datasFetched, flights }) => <div className="main">
+	{/* datas fetched and flights to display */}
 	{ datasFetched && flights.length > 1 && <div className="container">
 		{ flights.map((flight, index) => <Flight key={ index } state={ flight } />) }
 	</div> }
 
+	{/* datas fetched but no flights to display */}
 	{ datasFetched && flights.length === 0 && <div className="no-datas">
 		<p>No datas from API. Please change your search's settings or retry in a few seconds.</p>
 	</div> }
 
+	{/* datas not fetched yet */}
 	{ !datasFetched && <div className="fetch-datas">
 		<div className="loader">
 			<div className="loader-animation" />
--- src/store/middlewares/api.js
+++ src/store/middlewares/api.js
@@ -5,7 +5,7 @@ import { FETCH_DATAS, fetchData as fetchDataAction } from "../actions";
 const apiMW = _ => next => async action => {
 	if (action.type === FETCH_DATAS) {
 		try {
-			const res = await axios.get("https://opensky-network.org/api/states/all?lamin=47.081149&lomin=-4.901407&lamax=48.896081&lomax=-1.014896");
+			const res = await axios.get(`https://${ process.env.REACT_APP_USERNAME }:${ process.env.REACT_APP_PASSWORD }@opensky-network.org/api/states/all?lamin=47.081149&lomin=-4.901407&lamax=48.896081&lomax=-1.014896`);
 			const flights = res.data.states.slice(0,20);
 			action = fetchDataAction(flights);
 		}
